795c04a583de1b88fcd148abcfbd475f6bea6105
Justyna Rachowicz <mrowa44@gmail.com>
2018-01-10 22:37:46 +0100

    Some table styles

--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import ClassListItem from 'components/ClassListItem';
 
+import styles from './ClassList.scss';
+
 class ClassList extends React.Component {
   render() {
     const { groupOne, groupTwo, data } = this.props;
@@ -9,14 +11,14 @@ class ClassList extends React.Component {
       return item.group === groupOne || item.group === groupTwo;
     });
     return (
-      <table>
+      <table className={styles.table}>
         <thead>
-          <tr>
-            <th>time</th>
-            <th>subject</th>
-            <th>group</th>
-            <th>lecturer</th>
-            <th>place</th>
+          <tr className={styles.tableHeading}>
+            <th>Time</th>
+            <th>Subject</th>
+            <th>Group</th>
+            <th>Lecturer</th>
+            <th>Place</th>
           </tr>
         </thead>
         <tbody>
